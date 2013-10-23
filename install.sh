df1e82ae5560f824b2186d3c4029787eb8dd44bc
Like-all <lucidlynx@yandex.ru>
2013-10-23 11:45:35 +0200

    Installer: condition fix

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 case `uname` in
     "Linux")
-        if !$(which git); then
+        if ! which git; then
             echo "Fail"
         else
             echo "Success"
         fi
         ;;
     "Darwin")
-        if !$(which -s git); then
+        if ! which -s git; then
             echo "Fail on Mac"
         else
             echo "Success on Mac"
