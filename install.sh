d614320ecd7fe82d0db57222f8603228054ca73d
Like-all <lucidlynx@yandex.ru>
2013-10-23 12:49:40 +0200

    Installer: install vimrc

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+function install_vimrc() {
+    ln -is ~/.config/dotfiles/vimrc ~/.vimrc
+}
+
+function install() {
+    install_vimrc
+}
 case `uname` in
     "Linux")
         if ! which git; then
@@ -18,9 +25,9 @@ esac
 if ! [[ -d ~/.config/dotfiles/.git ]]; then
     mkdir -p ~/.config/dotfiles
     git clone git://github.com/Like-all/dotfiles.git ~/.config/dotfiles
-
+    install
 else
     cd ~/.config/dotfiles && git pull
-
+    install
 fi
 
