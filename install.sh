3b1bfe23055b582223c8f3664c2a7487658e79f5
Like-all <lucidlynx@yandex.ru>
2013-10-23 11:40:12 +0200

    Installer: variable

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 
 case `uname` in
     "Linux")
-        if which git; then
+        if !$(which git); then
+            echo "Fail"
+        else
             echo "Success"
         fi
         ;;
     "Darwin")
-        if which -s git; then
+        if !$(which -s git); then
+            echo "Fail on Mac"
+        else
             echo "Success on Mac"
         fi
         ;;
