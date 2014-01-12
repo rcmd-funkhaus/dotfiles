2a61107c5a002974e2722d1d6357bbfd71d11229
Like-all <lik3a11@gmail.com>
2014-01-13 02:25:48 +0400

    Changed installing method

--- install.sh
+++ install.sh
@@ -4,30 +4,48 @@ function install_vimrc() {
     ln -is ~/.config/dotfiles/vimrc ~/.vimrc
 }
 
-function install() {
+function install_i3cfg() {
+    if ! [[ -d ~/.i3 ]]; then
+        mkdir ~/.i3
+    fi
+    ln -is ~/.config/dotfiles/i3/config ~/.i3/config
+}
+
+function clone_repo() {
+    if ! [[ -d ~/.config/dotfiles/.git ]]; then
+        mkdir -p ~/.config/dotfiles
+        git clone git://github.com/Like-all/dotfiles.git ~/.config/dotfiles
+    else
+        cd ~/.config/dotfiles && git pull
+    fi
+}
+
+function setup_linux() {
+    install_vimrc
+    install_i3cfg
+}
+
+function setup_osx() {
     install_vimrc
 }
+
 case `uname` in
     "Linux")
         if ! which git; then
             echo "Please, install git first"
             exit 1
         fi
+        clone_repo
+        setup_linux
         ;;
     "Darwin")
         if ! which -s git; then
             echo "Please, install git first"
             exit 1
         fi
+        clone_repo
+        setup_osx
         ;;
 esac
 
-if ! [[ -d ~/.config/dotfiles/.git ]]; then
-    mkdir -p ~/.config/dotfiles
-    git clone git://github.com/Like-all/dotfiles.git ~/.config/dotfiles
-    install
-else
-    cd ~/.config/dotfiles && git pull
-    install
-fi
 
