6032ad3558419685915da337354d233998bfc9fb
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2017-04-11 05:39:36 +0200

    Updated provisioning for debian

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -4,3 +4,4 @@ sudo apt-get -qq update
 sudo apt-get install git python-pip
 sudo pip install -Iv ansible==2.2.1.0
 git clone https://github.com/Like-all/dotfiles.git
+/home/vagrant/dotfiles/deploy-dotfiles
