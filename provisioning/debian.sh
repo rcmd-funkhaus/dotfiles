2633f7f4e547faeac435ac7120e2211f14fb8709
Like-all <like-all@yandex.com>
2017-04-11 05:44:53 +0200

    Upgrade pip before installing ansible

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get -qq update
 sudo apt-get -y install git python-pip
+sudo pip install --upgrade pip
 sudo pip install -Iv ansible==2.2.1.0
 git clone https://github.com/Like-all/dotfiles.git
 /home/vagrant/dotfiles/deploy-dotfiles
