f2ee730200e4b63162aa9424a28b046e405bac01
Like-all <like-all@yandex.com>
2017-04-11 05:49:26 +0200

    Provide libyaml-dev before installing python dependencies

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get -qq update
-sudo apt-get -y install git python-pip
+sudo apt-get -y install git libyaml-dev python-pip
 sudo pip install --upgrade pip
 sudo pip install -Iv ansible==2.2.1.0
 git clone https://github.com/Like-all/dotfiles.git
