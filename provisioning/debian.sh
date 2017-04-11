185d4f78228fa43e5fc629f9c4e42303fcd701c0
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2017-04-11 06:12:15 +0200

    Invoke ansible directly

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -5,4 +5,4 @@ sudo apt-get -y install git libyaml-dev libpython2.7-dev libssl-dev python-pip
 sudo pip install --upgrade pip
 sudo pip install -Iv ansible==2.2.1.0
 git clone https://github.com/Like-all/dotfiles.git
-/home/vagrant/dotfiles/deploy-dotfiles
+cd /home/vagrant/dotfiles && ansible-playbook -i inventories/hosts dotfiles.yml -c local --diff
