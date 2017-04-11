ad564567649ac5dba0d12ce04f8dce3d276bc085
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2017-04-11 06:26:02 +0200

    Provisioning should run under vagrant user

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -5,4 +5,4 @@ sudo apt-get -y install git libyaml-dev libpython2.7-dev libssl-dev libffi-dev p
 sudo pip install --upgrade pip
 sudo pip install -Iv ansible==2.2.1.0
 git clone https://github.com/Like-all/dotfiles.git
-cd /home/vagrant/dotfiles && ansible-playbook -i inventories/hosts dotfiles.yml -c local --diff
+cd /home/vagrant/dotfiles && su vagrant -c "ansible-playbook -i inventories/hosts dotfiles.yml -c local --diff"
