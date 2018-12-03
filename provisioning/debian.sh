4d7d28fe61bbf407ffe431f559b322d9be6229dc
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-12-03 19:54:35 +0100

    Updated ansible version

--- provisioning/debian.sh
+++ provisioning/debian.sh
@@ -3,6 +3,6 @@
 sudo apt-get -qq update
 sudo apt-get -y install git libyaml-dev libpython2.7-dev libssl-dev libffi-dev python-pip
 sudo pip install --upgrade pip
-sudo pip install -Iv ansible==2.2.1.0
+sudo pip install -Iv ansible==2.7.4.0
 git clone https://github.com/Like-all/dotfiles.git
 cd /home/vagrant/dotfiles && su vagrant -c "ansible-playbook -i inventories/hosts dotfiles.yml -c local --diff"
